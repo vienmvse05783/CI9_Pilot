267c0d39927728b0fb2c7cc30a1b23b7b9e7b919
vienmvse05783 <35493276+vienmvse05783@users.noreply.github.com>
2018-09-10 20:16:41 +0700

    10/09

--- src/players/PLayerShoot.java
+++ src/players/PLayerShoot.java
@@ -6,27 +6,36 @@ import inputs.InputManager;
 
 public class PLayerShoot {
     private boolean shootLock = false;//shoot
-    FrameCounter frameCounter =  new FrameCounter(50);
+    FrameCounter frameCounter = new FrameCounter(50);
 
-    void run(Player player){
-        if(InputManager.instance.xPressed && !this.shootLock){
+    void run(Player player) {
+        if (InputManager.instance.xPressed && !this.shootLock) {
 //            Vector2D bulletPosition = player.position.subtract(0,0);
             // 1. try to recycle
-            PlayerBullet newBullet= GameObject.recycle();
-            if(newBullet== null){
-                newBullet = new PlayerBullet((int)player.position.x ,(int)player.position.y);
-            }else {
-                newBullet.isActive=true;
+            PlayerBullet newBullet = GameObject.recycle();
+            if (newBullet == null) {
+                newBullet = new PlayerBullet((int) player.position.x, (int) player.position.y);
+                GameObject.add(newBullet);
+
+            } else {
+                newBullet.isActive = true;
                 newBullet.position.x = player.position.x;
                 newBullet.position.y = player.position.y;
 
             }
             // 2. if recycle fail, create new.
-
-            GameObject.add(newBullet);
             this.shootLock = true;
+        }
+        if (shootLock) {
+            frameCounter.run();
+            if (frameCounter.expired) {
+                shootLock = false;
+                frameCounter.reset();
+            }
+
         }
 
     }
+
 
 }
